2bbcbcf0fb35b7b4baafd023b72ccaa53e08adbe
Sarah Roberts <sroberts687@gmail.com>
2018-09-18 20:33:26 -0400

    Moved homework_two files into subfolder from root directory.  Added skeleton to homework_three cs code.

--- homework_three/homework_three/Program.cs
+++ homework_three/homework_three/Program.cs
@@ -8,12 +8,35 @@ namespace homework_three
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+        public static class Globals
         {
+            public const double PI = 3.1415926;  // Unmodifiable
+            public static double dummy = -1; // Modifiable
 
-
-            Console.WriteLine("maybe?");
+            // search range for iterative methods
+            public static double min = -700;
+            public static double max = 700;
 
         }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homework two, Sarah Roberts.");
+
+
+
+
+
+
+
+            // keep window open
+            Console.WriteLine();
+            Console.WriteLine("\n Press any key to exit.");
+            Console.ReadKey();
+        }
+
     }
 }
