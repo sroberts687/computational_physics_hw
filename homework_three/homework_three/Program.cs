95b142ac103398aff716793888be8564af55ac53
Sarah Roberts <sroberts687@gmail.com>
2018-09-27 20:36:01 -0400

    defined lorentzian and gaussian fns

--- homework_three/homework_three/Program.cs
+++ homework_three/homework_three/Program.cs
@@ -12,7 +12,8 @@ namespace homework_three
 
         public static class Globals
         {
-            public const double PI = 3.1415926;  // Unmodifiable
+            public const double PI = 3.14159265358979323846264338; // Unmodifiable
+            public const double E =  2.71828182845904523536028747; // Unmodifiable
             public static double dummy = -1; // Modifiable
 
             // search range for iterative methods
@@ -37,6 +38,25 @@ namespace homework_three
             Console.WriteLine("\n Press any key to exit.");
             Console.ReadKey();
         }
+
+        static double L( double v, double alpha)
+        {
+            double v0 = 1.2;    // TODO: DEFINE V0
+            return alpha / (Globals.PI * Math.Pow(v - v0, 2) + Math.Pow(alpha, 2));
+        }
+
+        static double G( double v, double alpha)
+        {
+            double v0 = 1.2;    // TODO: DEFINE V0
+            double temp = 1 / alpha;
+            double exp = -Math.Log(2) * Math.Pow(v - v0, 2) / (alpha*alpha);
+            temp *= Math.Sqrt(Math.Log(2) / Globals.PI);
+            temp *= Math.Pow(Globals.E, exp);
+
+            return temp;
+        }
+
+
 
     }
 }
