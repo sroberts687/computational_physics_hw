ad636a5f2b1cb76b8bbfd968404c207781be5021
Sarah Roberts <sroberts687@gmail.com>
2018-10-21 21:00:57 -0400

    Implemented two versions of the MC Integration algorithm.  Version copied from website cited in earlier log doesn't work for functions with negative values.  New implementation works for both and follows course notes (much) more closely.  Old implementation will be removed in future commits.

--- midterm/midterm.py
+++ midterm/midterm.py
@@ -1,42 +1,91 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import random
 import pandas as pd
 
-def easy_function(x):
-    return((3)*(x**2))
+def g(x):   # g(x) = 2 x
+    return(2*x)
+def G(x):   # 1/2*x^2
+    return (0.5*x*x)
 
 def hard_function(x):
     return((1/np.sqrt(2*np.pi))*np.exp(-(x**2)/2))
 
 def f(x):
-    return x**2 + 2
+    return x**2 + -2
 
-def integrate(x1,x2,func,n):
+def F(x):
+    return (1/3)*x**3-2*x
+
+def integratePos(x1,x2,func,n):
 
     xvals = np.linspace(x1, x2, n)
     ymin = min(f(xvals)) - 1
     ymax = max(f(xvals)) + 1
-    
-    xout = []
-    yout = []
-    check = []
-    
+
+    xout = []   # list of x vals from RNG
+    yout = []   # list of y vals from RNG
+    ratio = []  # ratio of in-bounds vs. out-of-bounds values
+
     area=(x2-x1)*(ymax-ymin )
-    
+
     for i in range(n):
         x = random.uniform(x1,x2)
         xout.append(x)
-        y = random.uniform(0,f(x2))
+        y = random.uniform(ymin,func(x2))
         yout.append(y)
         if abs(y)>abs(func(x)) or y<0:
-          check.append(0)     
+          ratio.append(0)
         else:
-          check.append(1)
-          
-    #print(np.mean(check)*area)
-    return(np.mean(check)*area,xout,yout,check)
+          ratio.append(1)
+              
 
+    #print(np.mean(check)*area)
+    return(np.mean(ratio)*area,xout,yout)
+    
+    
+def integrate(x1,x2,func,n):
+    
+    integral = 0
+    
+    for i in range(0, n):
+        integral += func(random.uniform(x1,x2))
+    
+    
+    integral = (x2-x1) * integral / n
+    
+    return integral
+
+# numerical derivatives
+def NDeriv(idx, xvals):
+    deriv = 0
+    # see if we are at the front of the list
+    if idx == 0:
+        if ( idx == 0 and len(xvals) > 2):
+            deriv = -3/2*xvals[0] + 2*xvals[1] - 1/2*xvals[2]
+        elif ( idx == 0 and len(xvals) > 1 ):
+            deriv = -1*xvals(0) + 1*xvals(1)
+        else:
+            #only one point in xvals -- so no deriv can be calculated
+            deriv = 0
+    # see if we are at the end of the list
+    elif idx == len(xvals) and len(xvals == 2):
+        deriv = -1* xvals[idx-1] + 1* xvals[idx]
+    elif idx == len(xvals) and len(xvals) > 2:
+        deriv = 1/2*xvals[idx-2] -2*xvals[idx-1] + 3/2*xvals[idx]
+    elif len(xvals) >= 5:   # try five-point stencil
+        deriv = 1/12*xvals[idx - 2] - 2/3*xvals[idx-1]
+        + 2/3*xvals[idx+1] - 1/12*xvals[idx + 2]
+    else:   # middle point of a list with three vals
+        deriv = -1/2*xvals[idx-1] + 1/2*xvals[idx+1]
+
+
+    # valid for first derivative only
+    deriv /= abs(xvals[0] - xvals[1])
+    return deriv
+
+def TwoPtDeriv( y1, y2, dx):
+    return (y2-y1)/dx
 
 
 
@@ -50,7 +99,7 @@ y1 = f(x1)
 y2 = f(x2)
 
 #logical for development, if .true. then time-intensive code does not run
-saveTime = True
+saveTime = False    #True
 
 
 #plot function
@@ -58,23 +107,23 @@ saveTime = True
 #plt.plot(xvals,f(xvals))
 #plt.show()
 
-print("int(f(x) from -1 to 1 is 4.666666666....") 
-true = 4.66666666666666667
-val = integrate(-1, 1, f, 100)[0]
+true = F(1)-F(-1)
+print("int(f(x) from -1 to 1 is ", true)
+val = integrate(-1, 1, f, 100)
 print("estimate for n = 100 is ", val )
 print("\t error is", abs(true-val)/true*100, "%")
-val = integrate(-1,1, f, 1000)[0]
+val = integrate(-1,1, f, 1000)
 print("estimate for n = 1 000 is ", val)
 print("\t error is", abs(true-val)/true*100, "%")
 
 if saveTime == False:
-    val1 = integrate(-1, 1, f, 1000000)[0]
+    val1 = integrate(-1, 1, f, 1000000)
     print("1st estimate for n = 1 000 000 is ",  val1)
     print("\t error is", abs(true-val1)/true*100, "%")
-    val2 = integrate(-1, 1, f, 1000000)[0]
+    val2 = integrate(-1, 1, f, 1000000)
     print("2nd estimate for n = 1 000 000 is ",  val2)
     print("\t error is", abs(true-val2)/true*100, "%")
-    val3 = integrate(-1, 1, f, 1000000)[0]
+    val3 = integrate(-1, 1, f, 1000000)
     print("3rd estimate for n = 1 000 000 is ",  val3)
     print("\t error is", abs(true-val3)/true*100, "%")
     print()
@@ -85,15 +134,83 @@ if saveTime == False:
 
 print()
 print()
-print("int(f(x) from -2 to 4 is 36....") 
-true = 36.0
-val = integrate(-2, 4, f, 1000)[0]
+true = F(4)-F(-2)
+print("int(f(x) from -2 to 4 is ", true)
+val = integrate(-2, 4, f, 1000)
 print("estimate for n = 1 000 is ", val )
 print("\t error is", abs(true-val)/true*100, "%")
-val = integrate(-2, 4, f, 10000)[0]
+val = integrate(-2, 4, f, 10000)
 print("estimate for n = 10 000 is ", val)
 print("\t error is", abs(true-val)/true*100, "%")
-val = integrate(-2, 4, f, 1000000)[0]
+val = integrate(-2, 4, f, 1000000)
 print("estimate for n = 100 000 is ", val)
 print("\t error is", abs(true-val)/true*100, "%")
+
+
+# TODO: IMPORTANCE SAMPLING (from class)
+
+
+# TODO: REJECTION TECHNIQUE (from class)
+
+
+# TODO: STRATIFIED SAMPLING
+    # 1. divide integration domain into k regions
+    # 2. select n_k num points for MC technique in eac h region
+    # 3. apply monte carlo integration to each of the regions
+
+print()
+print("Improvement: divide the domain into regions where the function ")
+print("has less variance.  Integrate over these regions so that fewer ")
+print("samples are needed, improving the speed of the calculation.  This")
+print("method also lends itself well to parallelization, where division ")
+print("of the domain could be run on seperate threads.  This is not ")
+print("implemented here, because the overhead of creating threads in python")
+print("would not be a good trade-off for the simple functions and small ")
+print("regions defined in this driver program.")
+print()
+
+k = 3
+xa = -2
+xb = 4
+n = 10000
+vals = []
+step = (xb - xa) / k
+thisA = xa
+thisB = 0
+for i in range(0, k) :
+    thisB = thisA + step
+    print("i = ", i)
+    print("a = ", thisA )
+    print("b = ", thisB)
+    vals.append(integrate(thisA, thisB, f, n))
+    print("int = ", vals[i])
+    thisA = thisA + step
+res = 0.0
+for i in range(0, len(vals)):
+    res = res + vals[i]
+print("MC w/o steps int of f from ", xa, " to ", xb, " is ",
+      integrate(xa, xb, f, n*k) )
+print("MC w/ steps int of f from ", xa, " to ", xb, " is ", res )
+print("actual int of f from", xa, " to ", xb, " is ", F(xb)-F(xa)  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
