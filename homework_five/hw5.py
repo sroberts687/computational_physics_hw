f162928f395583c66edda6254ca883a7936b65c2
sarah <sroberts687@gmail.com>
2018-11-11 19:06:43 -0500

    outlined hw5 PDE assignment

---
+++ homework_five/hw5.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+HW5 Python Script
+
+Sarah Roberts
+"""
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+#import scipy.interpolate
+
+def FTCS():
+    
+    return someVar
+    
+def Lax():
+    
+    return someVar
+
+def LaxWendroff():
+    
+    return someVar
+
+
+print("Homework Five")
+print("FTCS solution to the traffic problem")
+print("3D plot of density vs. position and time")
+print()
+
+
+print("Lax solution to the traffic problem")
+print("3D plot of density vs. position and time")
+print()
+
+print("Lax-Wendroff solution to the traffic problem")
+print("3D plot of density vs. position and time")
+print()
+
+print
+
+
+
+#plt.plot(nVals, eErrorVals)
+#plt.plot(nVals, rkErrorVals)
+##plt.savefig('C:\\Users\\srobe\\Desktop\\eulerRKErrors.png')
+#plt.close()
+
