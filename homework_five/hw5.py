4cfcf2ff8925d9324becb4da0020a930b7e1d3e6
Sarah Roberts <sroberts687@gmail.com>
2018-11-14 08:04:34 -0500

    update hw5.py
    actually figured out what problem we have to solve

--- homework_five/hw5.py
+++ homework_five/hw5.py
@@ -9,6 +9,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import scipy.interpolate
 
+### Notes on traffic flow problem
+### traffic flow
+###     v = v_m ( 1 - p / p_m )   if p < p_m
+###     v = 0                     if p >= p_m 
+###     v_m is max speed 
+###     p_m is max density
+### Burger's Equation 
+###     dp/dt = - d/dx((a + 1/2 * b * p)p)
+###     a = v_m and b = -2*v_m/p_m
+### We're actually looking to solve non-linear advection equtation
+###     dp/dt = -c(p)*dp/dx
+###     c(p) = v_m(1-2*p/p_m)
+###     p(x, t=0) = p_0(x)
+
+
+
 def FTCS():
     
     return someVar
